a65f38bcca657a03a00f81578545950522a7d4da
jaredwilli <jaredwilli@gmail.com>
2012-11-24 08:17:59 -0500

    updated some stuff and deleted some stuff

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 
 // Overrides the injected script from cellphone carrier while phone is tethered
 bmi_SafeAddOnload = null;
+
+
+var sect = document.getElementsByTagName('section');
+
+for (var i = 0; i < sect.length; i++) {
+	var h2 = sect[i].querySelectorAll('h2');
+
+	for (var j = 0; j < h2.length; j++) {
+		console.log(h2[j]);
+
+		h2[j].addEventListener('hover', hoverLink, false);
+	}
+}
+
+function hoverLink(e) {
+	console.log(e);
+};
